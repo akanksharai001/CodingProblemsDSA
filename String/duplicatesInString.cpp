70e9d9a2ecf7773432026753d8fa7858bc447bd1
Akanksha Rai <akanksharai0707@gmail.com>
2021-07-06 17:33:08 +0530

    initial commit

---
+++ String/duplicatesInString.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include<string.h>
+
+using namespace std;
+
+int main()
+
+{
+
+    char string[80];
+
+    cout<<"Enter string: ";
+
+    cin.getline(string,80);
+
+    cout<<"Duplicate characters are following"<<endl;
+    
+    for(int i=0;i<strlen(string);i++){                  
+        for(int j=i+1;j<strlen(string);j++){
+
+            if(string[i]==string[j]){  
+                
+                  cout<<string[i]<<" "<<c<<endl;                
+
+                              
+
+                break;
+
+            }
+                         
+
+
+        }
+
+    }
+
+    return 0;
+
+}
